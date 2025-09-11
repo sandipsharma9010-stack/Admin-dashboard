e027dda9e4128e36d4e8a0ccaae5fe7b80d75a6f
sandeeptech <sandeep.sharma@dpdpconsultants.com>
2025-09-11 17:27:55 +0530

    crud opeartion add

--- src/components/Crud/FormComponent.jsx
+++ src/components/Crud/FormComponent.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const FormComponent = ({ form, handleChange, handleSubmit, editIndex, data }) => {
   return (
-    <div className="max-w-2xl mx-auto p-4 bg-white rounded shadow-md mt-10">
+    <div className="max-w-9xl mx-auto p-4 bg-white rounded shadow-md mt-10">
       <form onSubmit={handleSubmit} className="space-y-4">
         <div>
           <label className="block text-sm font-medium text-gray-700">Name</label>
